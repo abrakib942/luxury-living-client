6d87eb76191474c83a7cec87f427bf29e067f11b
abrakib942 <abrakib942@gmail.com>
2022-06-27 19:02:48 +0600

    contact done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "./Banner";
+import Contact from "./Contact";
 import Projects from "./Projects";
 import Services from "./Services";
 import Testimonials from "./Testimonials";
@@ -11,6 +12,7 @@ const Home = () => {
       <Projects />
       <Services />
       <Testimonials />
+      <Contact />
     </div>
   );
 };
